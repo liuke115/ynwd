b6a8ff2284c24998e7d6eb97c9cf62b237cdcb1a
liuke <liuke115@hotmail.co.jp>
2018-07-16 11:16:31 +0800

    去除滚动条

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ html,body,#app,#inspire{
   height: 100%;
   padding: 0;
   margin: 0;
+  overflow: hidden;
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 
 }
